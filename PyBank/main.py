3ae238a50e33afa00ad3f538dce8c3a9eed74206
Cipolle <cipolle@Cipolles-MacBook-Pro.local>
2020-11-22 17:01:16 -0800

    outputs to terminal

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,9 @@
+#import dependencies
 import os
 import csv
 
+
+# count the number of rows exluding header
 csvpath = os.path.join("Resources","budget_data.csv")
 print(csvpath)
 count = 0
@@ -11,32 +14,55 @@ with open(csvpath, newline='') as csvfile:
         count = count + 1
 print(count)
 
+# sum the totals of the second column 
 profit = 0
-
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     for row in csvreader:
             profit = profit + int(row[1])
-
-
+#initialize two lists for next month and this month
 first_month = [0]
 next_month = []
 with open(csvpath, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
     for row in csvreader:
-        # avg_change_profit = sum(int(row[1]) - int((row - 1)[1]))/86
         first_month.append(row[1])
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)    
     for row in csvreader:
         next_month.append(row[1])
+#make sure each list is the same length
 next_month.append(0)
 
-print(first_month)
-print(next_month)
+# # check month lists if need be
+# print(f"first month {first_month}")
+# print(f"next month {next_month}")
+
+#initialize list for tracking monthly changes
+change_profit = []
+#fill list with differences from next month to this month
+for i in range(len(next_month)):
+    change_profit.append(int(next_month[i]) - int(first_month[i]))
+
+#remove first and last items of list of profit changes
+change_profit.pop(0)
+change_profit = change_profit[:-1]
+
+# # check the full list of monthly changes to profit
+# print(f"Monthly list of change in profit = {change_profit}")
+
+#Define avg, min and max monthly profit changes
+avg_change_profit = sum(change_profit) / len(change_profit)
+max_change_profit = max(change_profit)
+min_change_profit = min(change_profit)
+
+# print to terminal
 
 
-#define analysis for outputs
+#total months
 total_months = count
 print(f"Total Months = {count}")
 
@@ -45,12 +71,13 @@ total_profit = profit
 print(f"Total Profit = {profit}")
 
 # avg_change_profit = 
-print(f"Average Monthly Change In Profit = {avg_change_profit}")
+print(f"Average Monthly Change in Profits = {avg_change_profit}")
+
 # max_inc_profit =
-
+print(f"Maximum Monthly Increase in Profits = {max_change_profit}")
 # max_dec_losses = 
+print(f"Maximum Monthly decrease in Profits = {min_change_profit}")
 
-# print to terminal
 
 # export as a .txt text file
 
