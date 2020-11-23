1e45e616ebf7ee480c3d554f84eeee65d90c79a3
Cipolle <cipolle@Cipolles-MacBook-Pro.local>
2020-11-22 16:10:04 -0800

    month lists appended

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,7 +19,23 @@ with open(csvpath, newline='') as csvfile:
     for row in csvreader:
             profit = profit + int(row[1])
 
-    
+
+first_month = [0]
+next_month = []
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        # avg_change_profit = sum(int(row[1]) - int((row - 1)[1]))/86
+        first_month.append(row[1])
+    for row in csvreader:
+        next_month.append(row[1])
+next_month.append(0)
+
+print(first_month)
+print(next_month)
+
+
 #define analysis for outputs
 total_months = count
 print(f"Total Months = {count}")
@@ -29,7 +45,7 @@ total_profit = profit
 print(f"Total Profit = {profit}")
 
 # avg_change_profit = 
-
+print(f"Average Monthly Change In Profit = {avg_change_profit}")
 # max_inc_profit =
 
 # max_dec_losses = 
