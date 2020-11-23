e91dc236844e5706440fa2c125ae97f3495811dd
Cipolle <cipolle@Cipolles-MacBook-Pro.local>
2020-11-22 18:07:50 -0800

    votes tabulated

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -24,3 +24,37 @@ with open(csvpath, newline='') as csvfile:
 candidates_set = set(candidates)
 print(candidates_set)
 
+
+khan = 0
+li = 0
+correy = 0
+otooley = 0
+
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        if row[2] == "Khan":
+            khan = khan + 1
+print(f"Khan: (percentage), ({khan}) votes")
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        if row[2] == "Li":
+            li = li + 1
+print(f"Li: (percentage), ({li}) votes")
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        if row[2] == "Correy":
+            correy = correy + 1
+print(f"Correy: (percentage), ({correy}) votes")
+with open(csvpath, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        if row[2] == "O'Tooley":
+            otooley = otooley + 1
+print(f"Correy: (percentage), ({otooley}) votes")
